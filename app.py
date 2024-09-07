c0af3f9e16a9c491d55249f2ea39d670470baa53
msmahikasingh18j@outlook.com <138625546+SinghMahika@users.noreply.github.com>
2024-09-07 20:27:22 +0530

    Updated the app

--- app.py
+++ app.py
@@ -11,10 +11,9 @@ scaler = pickle.load(open('scaling.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('templates\home.html')
+    return render_template('home.html')
 
 @app.route('/predict_api', methods = ['POST'])
-
 def predict_api():
     data = request.json['data']
     print(data)
@@ -22,7 +21,15 @@ def predict_api():
     new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
     output = classifier.predict(new_data)
     print(output[0])
-    return jsonify(output[0])
+    return jsonify(int(output[0]))
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    output = classifier.predict(final_input)[0]
+    return render_template('home.html', prediction_text = 'The Predicted Category is {}'.format(output))
+
 
 
 if __name__=="__main__":
